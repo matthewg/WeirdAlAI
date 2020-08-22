6556b6ca6f0f2424b7504397d58b8963dd1fd7a4
Matthew Sachs <matthewg@zevils.com>
2020-08-21 23:24:25 -0700

    Retweet

--- FetchTweetsTrigger.cs
+++ FetchTweetsTrigger.cs
@@ -15,16 +15,17 @@ namespace matthewg.WeirdAlAI
         [FunctionName("FetchTweetsTrigger")]
         public static void Run(
             [TimerTrigger("0 12 * * * *")]TimerInfo myTimer,
-            [Blob("state/max-tweet", FileAccess.ReadWrite)]ref string maxTweetBlob,
+            [Blob("state/max-tweet", FileAccess.Read)]string maxTweetIn,
+            [Blob("state/max-tweet", FileAccess.Write)]out string maxTweetOut,
             ILogger log)
         {
             Auth.SetCredentials(Utils.TwitterCredentials());
 
             long maxTweet = 0;
-            long.TryParse(maxTweetBlob, out maxTweet);
+            long.TryParse(maxTweetIn, out maxTweet);
 
             log.LogInformation($"Running tweet fetch from {maxTweet}...");
-            var searchParameter = new SearchTweetsParameters("'weird AI'")
+            var searchParameter = new SearchTweetsParameters("\"weird AI\"")
             {
                 Lang = LanguageFilter.English,
                 MaximumNumberOfResults = 100,
@@ -34,13 +35,16 @@ namespace matthewg.WeirdAlAI
             foreach (ITweet tweet in Search.SearchTweets(searchParameter))
             {
                 log.LogInformation($"Got tweet: {tweet.ToString()}");
+                var rtTask = tweet.PublishRetweetAsync();
+                rtTask.Wait();
+                log.LogInformation($"Task completed with status: {rtTask.Status}");
                 if (tweet.Id > maxTweet)
                 {
                     maxTweet = tweet.Id;
                 }
             }
-            maxTweetBlob = maxTweet.ToString();
-            log.LogInformation($"Finished with max tweet {maxTweetBlob}.");
+            maxTweetOut = maxTweet.ToString();
+            log.LogInformation($"Finished with max tweet {maxTweetOut}.");
         }
     }
 }
